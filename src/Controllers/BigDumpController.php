57315c319b5dd287404a3145e0cc6e0eb604dbe6
Ɛɔıs3 <8407711+w3spi5@users.noreply.github.com>
2025-12-06 23:49:55 +0100

    Multi-line INSERT Session Persistence bug

--- src/Controllers/BigDumpController.php
+++ src/Controllers/BigDumpController.php
@@ -237,9 +237,12 @@ class BigDumpController
             return;
         }
 
-        // Clear any pending query from previous import of this file
-        $sessionKey = 'bigdump_pending_' . md5($filename);
-        unset($_SESSION[$sessionKey]);
+        // Clear any pending query file from previous import of this file
+        $uploadsDir = $this->config->get('uploads_dir', 'uploads');
+        $pendingFile = $uploadsDir . '/.pending_' . md5($filename) . '.tmp';
+        if (file_exists($pendingFile)) {
+            @unlink($pendingFile);
+        }
 
         // Redirect to import
         $url = $this->request->getScriptUri() . '?' . http_build_query([
